9a646cb6b339ab2897e457a1822d195e9f7e1f66
finkiespitz <christianpatellis@gmail.com>
2024-10-12 14:38:24 -0500

    Modified HandControlTestOpMode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/HandControlTestOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/HandControlTestOpMode.java
@@ -5,36 +5,36 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.Servo;
 
-import org.firstinspires.ftc.teamcode.common.Robot;
-
 @TeleOp(name = "Hand Control Test", group = "Testing")
 public class HandControlTestOpMode extends LinearOpMode {
-  Robot robot = new Robot(this);
-
   Servo clawGrabServo = null;
 
-  Gamepad currentGamepad1;
-  Gamepad prevGamepad1;
+  Gamepad currentGamepad1 = new Gamepad();
+  Gamepad prevGamepad1 = new Gamepad();
+
 
   double clawClosePosition = 0;
 
   @Override
   public void runOpMode() {
     clawGrabServo = hardwareMap.get(Servo.class, "clawGrabServo");
-    clawGrabServo.setPosition(0.15);
+    clawGrabServo.setPosition(clawClosePosition);
     waitForStart();
+
     while (opModeIsActive()) {
-      prevGamepad1 = currentGamepad1;
-      currentGamepad1 = gamepad1;
+      prevGamepad1.copy(currentGamepad1);
+      currentGamepad1.copy(gamepad1);
       if (currentGamepad1.right_bumper && !prevGamepad1.right_bumper) {
         if (clawClosePosition == 0) {
-          clawClosePosition = 0.2;
+          clawClosePosition = 0.175;
         }
         else {
           clawClosePosition = 0;
         }
       }
       clawGrabServo.setPosition(clawClosePosition);
+      telemetry.addData("claw position", clawClosePosition);
+      telemetry.update();
     }
   }
 }
