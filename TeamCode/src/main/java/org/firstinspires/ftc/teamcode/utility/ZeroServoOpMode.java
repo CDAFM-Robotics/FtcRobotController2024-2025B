1aa1fee428c86c9038502845838513eb9a3c3514
jonat <jonathanzwitt@gmail.com>
2024-11-25 18:37:57 -0600

    Added control of claw

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utility/ZeroServoOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utility/ZeroServoOpMode.java
@@ -5,15 +5,34 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.Servo;
 
-@TeleOp(name = "Zero Servo", group = "utility")
+import org.firstinspires.ftc.teamcode.common.Robot;
+
+@TeleOp(name = "Test Servo", group = "utility")
 public class ZeroServoOpMode extends LinearOpMode {
-  Servo clawGrabServo = null;
+  Servo servo = null;
+  Robot robot = new Robot(this);
+  double servoPosition = 0;
   @Override
   public void runOpMode() throws InterruptedException {
-    clawGrabServo = hardwareMap.get(Servo.class, "clawGrabServo");
+    servo = hardwareMap.get(Servo.class, "clawGrabServo");
 
-    clawGrabServo.setPosition(0);
+    servo.setPosition(0);
 
     waitForStart();
+
+    while (opModeIsActive()) {
+      robot.updateGamepads();
+
+      if (robot.currentGamepad2.a && !robot.previousGamepad2.a) {
+        servoPosition += 0.025;
+        servo.setPosition(servoPosition);
+      }
+      if (robot.currentGamepad2.b && !robot.previousGamepad2.b) {
+        servoPosition -= 0.025;
+        servo.setPosition(servoPosition);
+      }
+      telemetry.addData("Position", servoPosition);
+      telemetry.update();
+    }
   }
 }
